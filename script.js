0562ac68237056811dedd1b52f9196c1bb9c2f14
Mario Dickerson <dai.dickerson@gmail.com>
2023-07-24 20:35:04 -0500

    bonus 2 done

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ window.addEventListener("load", function(){
     fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response) {
         response.json().then(function (json) {
             const container = document.getElementById("container");
+            json.sort(function(a, b){return b.hoursInSpace-a.hoursInSpace});
+
+
             let i = 0;
 
             function skillz(json) {
@@ -13,21 +16,39 @@ window.addEventListener("load", function(){
                 return skillList
             }
 
+            function greenActive(json) {
+                let active = "";
+                if (json[i].active === true){
+                     active = `<li id="active">Active: true</li>`
+                } else {
+                    active = `<li>Active: ${json[i].active}</li>`
+                }
+                return active
+            }
+
             while (i < json.length) {
+                
+                let active = "active";
+                if (json[i].active === "true"){
+                    active = "active";
+                } else {
+                    active = "inactive";
+                }
+
                 container.innerHTML += `
                 <div class="astronaut">
                     <div class="bio">
                         <h3>${json[i].firstName} ${json[i].lastName}</h3>
                             <ul>
                                 <li>Hours in space: ${json[i].hoursInSpace}</li>
-                                <li>Active: ${json[i].active}</li>
+                                ${greenActive(json)}
                                 <li>Skills: ${skillz(json)}</li>
                             </ul>
                     </div>
                     <img class="avatar" src=${json[i].picture}>
                 </div>
                 `;
-                
+
                 i++;
             }
             
